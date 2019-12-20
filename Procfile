2212dc51e8cd4bb2e30cf0a2e5a86413c6fb7bbc
thewerefriend <samtreeves@gmail.com>
2019-12-20 15:51:56 +0000

    heroku, second attempt

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python3 bot.py messages.txt
+web: python3 server.py
---
+++ server.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+app.run(environ.get('PORT'))
